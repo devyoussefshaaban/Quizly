a585c04bfe2e2fa45b033f6d95da3d9715d86362
unknown <imdevyoussef@gmail.com>
2025-12-16 09:23:55 +0200

    server: db config updated

--- packages/backend/configs/dbConfig.ts
+++ packages/backend/configs/dbConfig.ts
@@ -2,8 +2,9 @@ import { connect } from "mongoose"
 import { isResponseError } from "../interfaces/errorInterface"
 
 export const connectDB = async () => {
+    const isDev = process.env.NODE_ENV === "development"
     try {
-        await connect(process.env.MONGO_URI || "")
+        await connect(isDev ? process.env.MONGO_URI || "" : process.env.MONGO_PRODUCTION_URI || "")
     } catch (error) {
         if (isResponseError(error)) {
             console.error("MONGO DB CONNECTION ERROR", error.message)
